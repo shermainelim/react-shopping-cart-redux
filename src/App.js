66f6be0bed13f4be74e1ac625bc97e4d38d703ea
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-25 12:22:30 +0800

    Commit 1
    migration

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop";
+import Checkout from "./routes/checkout/checkout";
+
+// const Shoppes = () => {
+//   return (<Shop/>)
+// };
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
