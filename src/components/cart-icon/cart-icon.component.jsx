07a51806a631cd9c2919a78dcab1b825060759bb
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-26 13:08:24 +0800

    add to cart migration context to redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,7 +9,6 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
-  // const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   const dispatch = useDispatch();
   const cartCount = useSelector(selectCartCount);
   const isCartOpen = useSelector(selectIsCartOpen);
