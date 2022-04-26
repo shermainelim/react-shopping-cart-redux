4af6df6f2d309e1b723217cc96f370f2ed983d83
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-26 13:02:28 +0800

    migrating cart context to redux
    open and close the cart basket

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,23 +1,25 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { CartContext } from '../../contexts/cart.context';
+import { useSelector } from "react-redux";
 
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from "../../store/cart/cart.selector";
+
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
-} from './cart-dropdown.styles';
+} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
+  const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
-    navigate('/checkout');
+    navigate("/checkout");
   };
 
   return (
